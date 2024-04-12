671c86907964c8d254667369d5cf81c636ff2c28
naomipearse <naomi.pearse@dae.mn>
2024-04-12 13:33:12 +0100

    Json dependancy added

---
+++ JSONParse.java
@@ -0,0 +1,9 @@
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+public class JSONParse {
+    public JSONParse(String jsonString){
+        JSONObject jsonObject = new JSONObject();
+        System.out.println(jsonObject);
+    }
+}
