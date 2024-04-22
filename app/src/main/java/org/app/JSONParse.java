b6fd93091a65c4bf95a414f28000ffedd0fe92fd
naomipearse <naomi.pearse@dae.mn>
2024-04-22 13:08:35 +0100

    Jackson implemented with JsonNode. Sunrise data printed out

--- app/src/main/java/org/app/JSONParse.java
+++ app/src/main/java/org/app/JSONParse.java
@@ -1,9 +1,5 @@
 package org.app;
 
-import org.json.simple.parser.ContainerFactory;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
-
 import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
@@ -12,49 +8,22 @@ import java.lang.reflect.*;
 
 import javax.swing.plaf.synth.SynthTextAreaUI;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class JSONParse {
     public static void getJSON(String jsonString){
         System.out.println("getJSON Running");
-        JSONParser parser = new JSONParser();
-        ContainerFactory containerFactory = new ContainerFactory() {
-            @Override
-            public Map createObjectContainer() {
-                return new LinkedHashMap<>();
-            }
-            @Override
-            public List creatArrayContainer() {
-                return new LinkedList<>();
-            }
-        };
-
+        ObjectMapper objectMapper = new ObjectMapper();
         try {
-            Object returned = parser.parse(jsonString, containerFactory); 
-            Map<String, Object> mapReturned = mapReturned.getClass()
-            System.out.println("Map returned in OG state: " + mapReturned);
-
-            // String results = mapReturned.get("results").toString();
-            // System.out.println("RESULTS= " + results);
-
-            // Map<String, Object> resultsMap = (Map<String, Object>) parser.parse(results, containerFactory);
-
-            // System.out.println("RESULTSMAP= " + resultsMap);
-
-            // Object date = resultsMap.get("date");
-            // Object sunrise = resultsMap.get("sunrise");
-            // Object sunset = resultsMap.get("sunset");
-            
-            // System.out.println("");
-            // System.out.println("Here are the sunrise and sunset times for " + date + ".");
-            // System.out.println("Sunrise in your location is at: " + sunrise);
-            // System.out.println("Sunset in your location is at: " + sunset);
-        } catch (ParseException pe) {
-            System.out.println(pe);
-        } catch (Exception e){
+            JsonNode jsonNode = objectMapper.readTree(jsonString);
+            // Map<String, Object> map = objectMapper.readValue(jsonString, new TypeReference<Map<String, Object>>() {});
+            Object sunrise = jsonNode.get("results").get("sunrise").textValue();
+            System.out.println("SUNRISE:  " + sunrise);
+        } catch(Exception e){
             System.out.println(e);
         }
-
-        // System.out.println("JSONObject = ", jsonObject);
-        // System.out.println("Array", array);
-    }
+            
+    }       
 }
